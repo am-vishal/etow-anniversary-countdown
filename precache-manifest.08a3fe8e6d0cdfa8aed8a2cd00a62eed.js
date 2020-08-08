48a4ea8810450410b641fcf079fb7b637a606d45
Vishal Kumar <vishal.k0905@gmail.com>
2020-08-08 16:40:06 +0530

    Updates

---
+++ precache-manifest.08a3fe8e6d0cdfa8aed8a2cd00a62eed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8457934847cac06d79fa2f158caada7",
+    "url": "/etow-anniversary-countdown/index.html"
+  },
+  {
+    "revision": "3c8c75ee0b46889a4918",
+    "url": "/etow-anniversary-countdown/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dc0cd73a99c4d8db1d73",
+    "url": "/etow-anniversary-countdown/static/css/main.009ed6e1.chunk.css"
+  },
+  {
+    "revision": "3c8c75ee0b46889a4918",
+    "url": "/etow-anniversary-countdown/static/js/2.35f9ba37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/etow-anniversary-countdown/static/js/2.35f9ba37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0cd73a99c4d8db1d73",
+    "url": "/etow-anniversary-countdown/static/js/main.43dab14e.chunk.js"
+  },
+  {
+    "revision": "865ee1de065edcf37540",
+    "url": "/etow-anniversary-countdown/static/js/runtime-main.06e3559a.js"
+  },
+  {
+    "revision": "73dc4fdc5b73e7c20404e17271d4402a",
+    "url": "/etow-anniversary-countdown/static/media/abs1.73dc4fdc.jpg"
+  }
+]);
